26230fd9a4409d9300a8b0e1205b3f010e5fd77e
pavankhedkar4 <pavankhedkar4@gmail.com>
2026-02-24 20:08:10 +0530

    Initial Commit

---
+++ src/main/java/com/trade/app/login_service/TradeLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trade.app.login_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeLoginServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trade/app/login_service/TradeLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.app.login_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeLoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
